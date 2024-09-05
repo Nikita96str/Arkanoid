ee99aef8ff6bc71b021ff9e3f510337a7a230f83
unknown <barbashin_nd@lexema.ru>
2024-09-05 17:59:05 +0500

    Adding the platform

--- main.js
+++ main.js
@@ -1,23 +1,43 @@
-const canvasNode = document.getElementById('#arkanoid');
+const canvasNode = document.getElementById('arkanoid');
 const canvasContext = canvasNode.getContext('2d')
 
 PADDLE_WIDTH = 50
 PADDLE_HEIGHT = 10
 BALL_RADIUS = 10
+PADDLE_X = canvasNode.width / 2
+PADDLE_Y = canvasNode.height - PADDLE_HEIGHT*2
+
 canvasContext.fillStyle = '#0095DD'
 
-function drawPaddle() {
-    canvasContext.beginPath()
-    canvasContext.rect(canvasNode.width / 2
-        , canvasNode.height - PADDLE_HEIGHT
+function drawPaddle(corX, corY) {
+    canvasContext.beginPath()    
+    canvasContext.rect(corX
+        , corY
         , PADDLE_WIDTH
         , PADDLE_HEIGHT)
     canvasContext.fill()
-    canvasNode.closePath()
+    canvasContext.closePath()
 }
 
 function game() {
-    drawPaddle()
+    console.log("game start");
+    canvasContext.clearRect(0,0, canvasNode.width, canvasNode.height)
+    drawPaddle(PADDLE_X, PADDLE_Y)
+    console.log("game over");
+    
 }
 
 game()
+
+document.addEventListener('keyup', function movePaddle(event) {
+    console.log('keyup');
+    if (event.code == 'ArrowLeft') {
+        PADDLE_X -= 5
+        drawPaddle(PADDLE_X, PADDLE_Y)
+        console.log('PADDLE_X', PADDLE_X);
+    } else if(event.code == 'ArrowRight') {
+        PADDLE_X += 5
+        drawPaddle(PADDLE_X, PADDLE_Y)
+        console.log('PADDLE_X', PADDLE_X);
+    }
+})
