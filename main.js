dd6945ed764a400e9ecdd07c2f3c1b17eaa91537
unknown <barbashin_nd@lexema.ru>
2024-09-09 19:07:14 +0500

    Add text blocks and bricks

--- main.js
+++ main.js
@@ -1,17 +1,63 @@
 const canvasNode = document.getElementById('arkanoid');
 const canvasContext = canvasNode.getContext('2d')
 
+let play = false;
+let score = 0;
+let lives = 0;
 PADDLE_WIDTH = 50
-PADDLE_HEIGHT = 10
+PADDLE_HEIGHT = 8 
+BRICK_WIDTH = PADDLE_WIDTH * 1.5
+BRICK_HEIGHT = PADDLE_HEIGHT*2
+BRICKS_OFFSET = 10
 BALL_RADIUS = 6
+CENTER_X = canvasNode.width / 2
 PADDLE_X = canvasNode.width / 2
+CENTER_Y = canvasNode.height / 2
 PADDLE_Y = canvasNode.height - PADDLE_HEIGHT*2
 ballX = canvasNode.width / 2
 ballY = canvasNode.height / 2
 addX = 1
 addY = 1
-canvasContext.fillStyle = '#0095DD'
 
+bricksHorizontal = Math.floor(canvasNode.width / (BRICK_WIDTH + BRICKS_OFFSET)) 
+bricksOffset = (canvasNode.width - (bricksHorizontal * (BRICK_WIDTH + BRICKS_OFFSET)) + BRICKS_OFFSET)/2
+bricksVertical = 3
+bricksStatus = []
+
+const newArr = []
+for (let st = 0; st < bricksHorizontal; st++) {
+    newArr.push(1)
+}
+for (let i = 0; i < bricksVertical; i++) {
+    bricksStatus.push(newArr)
+}
+console.log('bricksOffset:', bricksOffset);
+console.log('bricksHorizontal:', bricksHorizontal);
+console.log('bricksStatus:', bricksStatus);
+
+function drawBricks() {
+    for (let index = 0; index < bricksStatus.length; index++) {
+        const arrBricks = bricksStatus[index]
+        const row = index + 1
+        for (let i = 0; i < arrBricks.length; i++) {
+            const status = i + 1;
+            // console.log('i:', i);
+            // console.log('status:', status);
+            //     console.log('x:', 
+            //         bricksOffset + BRICK_WIDTH * status + BRICKS_OFFSET*i);
+            
+            canvasContext.beginPath()    
+            canvasContext.rect(
+                bricksOffset + BRICK_WIDTH * i + BRICKS_OFFSET*i
+                , (BRICK_HEIGHT+BRICKS_OFFSET)*row 
+                , BRICK_WIDTH
+                , BRICK_HEIGHT)
+            canvasContext.fill()
+            canvasContext.closePath()
+            
+        }
+    }
+}
 function drawPaddle(corX, corY) {
     // canvasContext.clearRect(0,0, canvasNode.width, canvasNode.height)
     canvasContext.beginPath()    
@@ -55,31 +101,60 @@ function collideWalls() {
     ballY += addY
 }
 
+function maintextOnScreen(phrase, posY) {
+    canvasContext.fillStyle = "red";
+    canvasContext.strokeStyle = "#F00";
+    // canvasContext.font = "italic 10pt Arial";
+    canvasContext.textAlign = "center";
+    // canvasContext.strokeText(phrase, CENTER_X, CENTER_Y/2);
+    canvasContext.font = 'bold 20px sans-serif';
+    canvasContext.fillText(phrase, CENTER_X, posY);
+}
+
+function textOnScreen(phrase, posX, posY, size, align) {
+    canvasContext.fillStyle = "red";
+    canvasContext.strokeStyle = "#F00";
+    canvasContext.font = `bold ${size}pt Arial`;
+    canvasContext.textAlign = align;
+    canvasContext.fillText(phrase, posX, posY);
+}
+
+
 function game() {
     // console.log("game start");
+    canvasContext.fillStyle = 'blue'
     canvasContext.clearRect(0,0, canvasNode.width, canvasNode.height)
+    drawBricks()
     drawPaddle(PADDLE_X, PADDLE_Y)
     drawBall(ballX, ballY)
-
-    collideWalls()
+    textOnScreen('Счёт: ' + score, posX = 10, posY = 15, size = 10, align = 'start')
+    textOnScreen('Жизни: ' + lives, posX = canvasNode.width - 10, posY = 15, size = 10, align = 'end')
+    if (play) { 
+        collideWalls()
+    } else {
+        textOnScreen("Press 'Space' for start/pause game", posX = CENTER_X, posY = CENTER_Y/2, size = 20, align = 'center')
+    }
     // console.log("game over");
-
-    requestAnimationFrame(game);
-    
+    requestAnimationFrame(game);   
 }
 
 game()
 
 document.addEventListener('keydown', function movePaddle(event) {
     console.log('keydown');
-    if (event.code == 'ArrowLeft' && PADDLE_X > 0) {
-        PADDLE_X -= 5
-        drawPaddle(PADDLE_X, PADDLE_Y)
-        console.log('PADDLE_X', PADDLE_X);
-    } else if(event.code == 'ArrowRight' && PADDLE_X < canvasNode.width-PADDLE_WIDTH) {
-        PADDLE_X += 5
-        drawPaddle(PADDLE_X, PADDLE_Y)
-        console.log('canvasNode.width-PADDLE_WIDTH:', canvasNode.width-PADDLE_WIDTH);
-        console.log('PADDLE_X', PADDLE_X);
+    if(event.code == 'Space') {
+        play = !play
+    }
+    if (play) {
+        if (event.code == 'ArrowLeft' && PADDLE_X > 0) {
+            PADDLE_X -= 5
+            drawPaddle(PADDLE_X, PADDLE_Y)
+            console.log('PADDLE_X', PADDLE_X);
+        } else if(event.code == 'ArrowRight' && PADDLE_X < canvasNode.width-PADDLE_WIDTH) {
+            PADDLE_X += 5
+            drawPaddle(PADDLE_X, PADDLE_Y)
+            console.log('canvasNode.width-PADDLE_WIDTH:', canvasNode.width-PADDLE_WIDTH);
+            console.log('PADDLE_X', PADDLE_X);
+        }  
     }
 })
